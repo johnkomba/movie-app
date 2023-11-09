f444b53882457b5431f9a2d6a38b5daf9624922d
John Komba <john.mwangi1@student.moringaschool.com>
2023-11-09 10:34:14 +0300

    starter home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Movie App</h1>
+      <p>Discover and explore a wide range of movies.</p>
+    </div>
+  );
+}
+
+export default Home;
