def6fb5b63c9edd3c75f081fff26cba3c6979d0a
John Komba <john.mwangi1@student.moringaschool.com>
2023-11-09 10:44:33 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Search from './Search';
+import MovieDetail from './MovieDetail';
+import Favorites from './Favorites';
+import Watchlist from './Watchlist';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={MovieDetail} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/watchlist" component={Watchlist} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
