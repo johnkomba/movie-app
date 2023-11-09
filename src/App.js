eaf94c5749de9c645fc838d872adf4c0249de9da
John Komba <john.mwangi1@student.moringaschool.com>
2023-11-09 15:58:08 +0300

    adding more styling and functionalities

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import MovieDetail from './components/MovieDetail';
 import Favorites from './components/Favorites';
 import Watchlist from './components/Watchlist';
 import NotFound from './components/NotFound';
-
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/">Komba Movies App</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
@@ -35,6 +34,7 @@ function Navbar() {
     </nav>
   );
 }
+
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles.css'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../styles.css';
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const apiKey = 'fa82ab71';
+    axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=movie&page=${page}`)
+      .then(response => {
+        setMovies(response.data.Search);
+      })
+      .catch(error => {
+        console.error('Error fetching movies:', error);
+      });
+  }, [page]);
+
+  const handleNextPage = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(prevPage => prevPage - 1);
+    }
+  };
+
   return (
     <div className="container">
       <div className="header">
         <h1>Welcome to My Movie App</h1>
         <p>Discover and explore a wide range of movies.</p>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/favorites">Favorites</Link></li>
-        <li><Link to="/watchlist">Watchlist</Link></li>
-      </ul>
+
+      <div className="movie-list">
+        {movies.map(movie => (
+          <div key={movie.imdbID} className="movie-card">
+            <img src={movie.Poster} alt={movie.Title} />
+            <h3>{movie.Title}</h3>
+            <p>Year: {movie.Year}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={page === 1}>Previous</button>
+        <button onClick={handleNextPage}>Next</button>
+      </div>
     </div>
   );
 }
